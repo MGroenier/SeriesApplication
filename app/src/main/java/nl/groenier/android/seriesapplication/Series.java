6f441c90e7f7ec2f4327e9ede43455c8c8899c86
Martijn Groenier <martijn@groenier.nl>
2016-09-27 20:26:33 +0200

    Created a "Series"-model.

---
+++ app/src/main/java/nl/groenier/android/seriesapplication/Series.java
@@ -0,0 +1,22 @@
+package nl.groenier.android.seriesapplication;
+
+/**
+ * Created by Martijn on 27/09/2016.
+ */
+
+public class Series {
+
+    private String title;
+
+    public Series(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
